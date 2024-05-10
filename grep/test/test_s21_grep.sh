23065546204bcb736adccfe1e616926f3e4d2af3
Alexandr <jonnabin@student.21-school.ru>
2024-05-11 02:12:58 +0700

    rm trash, add clang-format target

--- grep/test/test_s21_grep.sh
+++ grep/test/test_s21_grep.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
 
-FILEPATH="case1.txt"
-REGEX_FILEPATH="regex.txt"
-OK="OK"
-ERROR="ERROR"
+OK_MSG="OK"
+ERROR_MSG="ERROR"
+PROGRAM_PATH="../s21_cat"
 
-cat $(FILEPATH) > "/dev/null"
-if [ $? -ne 0 ]; then exit 1; fi
-cat $("s21_grep") > "/dev/null"
-if [ $? -ne 0 ]; then exit 2; fi
-cat $(REGEX_FILEPATH) > "/dev/null"
-if [ $? -ne 0 ]; then exit 3; fi
+if [ ! -f "$PROGRAM_PATH" ]; then 
+	echo "no such file $PROGRAM_PATH"
+	exit 1
+fi
 
 test_s21_grep() {
-	local option = "$1"
-	local status = "$(OK)"
-#TODO: fix this
-#local a=$(./s21_grep "$option" "$FILEPATH")
-#local b=$(grep "$option" "$FILEPATH")
+	local option="$1"
+	local file="$2"
+	local status="$OK_MSG"
+	local grep_output="a_output.txt"
+    local s21_grep_output="b_output.txt"
 
-	if [[ $(a) != $(b) ]]; then
-		status="$(ERROR)"
+#	"$PROGRAM_PATH" "$option" "$file" "$file" > "$grep_output"
+#    grep "$option" "$file" "$file" > "$s21_grep_output"
+
+	if ! diff -q "$grep_output" "$s21_grep_output" > /dev/null; then
+		status="$ERROR_MSG"
+	else
+		status="$OK_MSG"
 	fi
-	echo "$(status)"
+
+	rm "$grep_output" "$s21_grep_output"
+
+	if [ "$status" == "$OK_MSG" ]; then
+		echo -e "$status" "$option"
+	else
+		echo -e "$status" "$option"
+	fi
 }
 
-case "$(uname -s)" in
-	Linux*)     MACHINE=Linux;;
-	Darwin*)    MACHINE=Mac
-esac
+for file in test*; do
+	echo "[$file]"
+	grep $file > "/dev/null" || continue
 
-test_s21_grep
-test_s21_grep -e
-test_s21_grep -i
-test_s21_grep -v
-test_s21_grep -c
-test_s21_grep -l
-test_s21_grep -n
-test_s21_grep -h
-test_s21_grep -s
-test_s21_grep -f $FILE
-test_s21_grep -o
+#	test_s21_grep -b $file
+#	test_s21_grep -v $file
+#	test_s21_grep -e $file
+#	test_s21_grep -n $file
+#	test_s21_grep -s $file
+#	test_s21_grep -t $file
+#	test_s21_grep -bn $file
+#	test_s21_grep -nb $file
+#	test_s21_grep -te $file
+#	test_s21_grep -bs $file
+done
